0d1ed2fcd1cb61b26a8c5c667a228016dde6d344
Zorgit <piligrimexe@gmail.com>
2023-09-29 00:03:42 +0300

    Refactoring method, add immutability

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -8,21 +8,34 @@ import java.util.ArrayList;
 final class App {
     private App() {
     }
-    public static List findWhere(final List<Map<String, String>> books,
-                                      final Map<String, String> where
+    public static List<Map<String, String>> findWhere(final List<Map<String,
+            String>> books, final Map<String, String> where
             ) {
-        List result = new ArrayList();
-        for (Map<String, String> map: books
-             ) {
-            var counter = 0;
+        List<Map<String, String>> result = new ArrayList<>();
+
+        books.forEach(map -> {
+            var matchingValuesCount = 0;
             for (String value: where.values()
-                 ) {
-                counter = map.containsValue(value) ? ++counter : counter;
+            ) {
+                matchingValuesCount = map.containsValue(value)
+                        ? ++matchingValuesCount : matchingValuesCount;
             }
-            if (counter == where.size()) {
+            if (matchingValuesCount == where.size()) {
                 result.add(map);
             }
-        }
+        });
+
+        /*
+        Additional solve:
+        books.forEach(map -> {
+            long matchingValuesCount = where.values().stream()
+                .filter(value -> map.containsValue(value))
+                .count();
+             if (matchingValuesCount == where.size()) {
+                result.add(map);
+             }
+         });
+         */
 
         return result;
     }
