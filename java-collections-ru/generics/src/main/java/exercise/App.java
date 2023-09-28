cdc672635a384c27397c32786d669e1008f5c401
Zorgit <piligrimexe@gmail.com>
2023-09-28 23:02:32 +0300

    Basic solve without Generic

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -3,8 +3,27 @@ package exercise;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
-import java.util.Map.Entry;
 
 // BEGIN
-
+final class App {
+    private App() {
+    }
+    public static List findWhere(final List<Map<String, String>> books,
+                                      final Map<String, String> where
+            ) {
+        List result = new ArrayList();
+        for (Map<String, String> map: books
+             ) {
+            var counter = 0;
+            for (String value: where.values()
+                 ) {
+                counter = map.containsValue(value) ? ++counter : counter;
+            }
+            if (counter == where.size()) {
+                result.add(map);
+            }
+        }
+        return result;
+    }
+}
 //END
---
+++ java-collections-ru/generics/src/main/java/exercise/Entry.java
@@ -0,0 +1,18 @@
+package exercise;
+
+public class Entry<T> {
+    private T value1;
+
+
+
+    public Entry(T value1, T value2) {
+        this.value1 = value1;
+    }
+
+    public T getValue1() {
+        return value1;
+    }
+
+
+
+}
