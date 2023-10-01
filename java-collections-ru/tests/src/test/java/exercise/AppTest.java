d8fc714888e81be50838f28fc8717c22257ed628
Zorgit <piligrimexe@gmail.com>
2023-10-01 19:34:29 +0300

    initial commit

---
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void testTake() {
+        // BEGIN
+        
+        // END
+    }
+}
