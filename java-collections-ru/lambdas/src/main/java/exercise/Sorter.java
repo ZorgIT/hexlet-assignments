6ee94a8ec3bed08f30ca602d16831e6b9ed7d9a6
Zorgit <piligrimexe@gmail.com>
2023-10-05 23:45:27 +0300

    Add method that takes oldest mans form list

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,15 +1,25 @@
 package exercise;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
-import java.util.function.Function;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 // BEGIN
 final class Sorter {
-
+    private Sorter() {
+    }
+    public static List takeOldestMans(final List<Map<String, String>> users) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        List<String> men = users.stream()
+                .filter(user -> user.get("gender").equals("male"))
+                .sorted(Comparator.comparing(user ->
+                        LocalDate.parse(user.get("birthday"), formatter)))
+                .map(user -> user.get("name"))
+                .collect(Collectors.toList());
+        return men;
+    }
 }
 // END
