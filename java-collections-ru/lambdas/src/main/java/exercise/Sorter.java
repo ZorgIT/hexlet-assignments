d19199be03796654af0740645a9a19cad0b8f304
Zorgit <piligrimexe@gmail.com>
2023-10-05 22:28:04 +0300

    init commit

---
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+// BEGIN
+final class Sorter {
+
+}
+// END
