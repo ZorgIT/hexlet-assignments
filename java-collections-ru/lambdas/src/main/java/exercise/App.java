c977acc577077ee69499e207a70edb4e9e53aafd
Zorgit <piligrimexe@gmail.com>
2023-10-14 00:39:51 +0300

    Simple solve for task

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -3,5 +3,24 @@ package exercise;
 import java.util.Arrays;
 
 // BEGIN
+class App {
+    public static String[][] enlargeArrayImage(String[][] image) {
+        int rows = image.length;
+        int cols = image[0].length;
 
+        String[][] enlargedImage = new  String[rows * 2][cols * 2];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                String pixel = image[i][j];
+                enlargedImage[i * 2][j * 2] =pixel;
+                enlargedImage[i * 2][j * 2 + 1] =pixel;
+                enlargedImage[i * 2 + 1][j * 2] =pixel;
+                enlargedImage[i * 2 + 1][j * 2 + 1] =pixel;
+
+            }
+        }
+        return enlargedImage;
+    }
+}
 // END
