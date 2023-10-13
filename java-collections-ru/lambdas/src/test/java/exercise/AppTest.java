84de1cb4e821f254e6cca22e6fc863ed0a264d3c
Zorgit <piligrimexe@gmail.com>
2023-10-14 00:49:30 +0300

    Check

--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -1,8 +1,39 @@
 package exercise;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 // BEGIN
+class AppTest {
+    @Test
+    void testEnlargeArrayImage() {
+        //Arrange
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
 
+        // Expected result
+        String[][] expectedEnlargedImage = {
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+        };
+
+        // Act
+        String[][] actualEnlargedImage = App.enlargeArrayImage(image);
+
+        // Assert
+        assertArrayEquals(expectedEnlargedImage, actualEnlargedImage);
+    }
+}
 // END
