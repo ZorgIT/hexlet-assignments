7af5199db02a7a85ac0d2420fb6d8ea635720a06
Zorgit <piligrimexe@gmail.com>
2023-10-21 01:41:14 +0300

    Add solve via stream

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,19 +1,29 @@
 package exercise;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
 // BEGIN
 class App {
-    public static void main(String[] args) {
+    public static String  getForwardedVariables(final String inputStr) {
+        List environments = Arrays.stream(inputStr.split("\n"))
+                .filter(line -> line.startsWith("environment="))
+                .map(line -> line.replace("environment=", "").trim())
+                .flatMap(line -> Arrays.stream(line.split(",")))
+                .map(line -> line.trim().replaceAll("\"| |,", ""))
+                .collect(Collectors.toList());
 
-    }
+        List<String> result = (ArrayList) environments.stream()
+                .filter(list -> list.toString().startsWith("X_FORWARDED_"))
+                .map(list -> list.toString().replaceFirst("X_FORWARDED_", ""))
+                .collect(Collectors.toList());
 
-    public static void  getForwardedVariables() {
-
+        return result.size() == 1 ? result.get(0) : result.stream()
+                .collect(Collectors.joining(","));
 
     }
 }
-
 
 //END
