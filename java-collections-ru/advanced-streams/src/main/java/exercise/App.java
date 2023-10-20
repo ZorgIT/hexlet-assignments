9029d522dcbbd8576afcc76fc31e103c92375e08
Zorgit <piligrimexe@gmail.com>
2023-10-20 23:46:47 +0300

    init commit

---
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -0,0 +1,19 @@
+package exercise;
+
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+// BEGIN
+class App {
+    public static void main(String[] args) {
+
+    }
+
+    public static void  getForwardedVariables() {
+
+
+    }
+}
+
+
+//END
