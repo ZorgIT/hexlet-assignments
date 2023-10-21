e196eab2a3c5d6afeb1db8c8c38d7d51ddfd208e
Zorgit <piligrimexe@gmail.com>
2023-10-21 21:22:10 +0300

    Stream API solve

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -1,40 +1,64 @@
 package exercise;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // BEGIN
 class App {
-    public static LinkedHashMap<String, String>  genDiff(Map<String, ?> dict1,
-                            Map<String, ?> dict2) {
-        //Объединенное множество ключей из двух словарей
-        Set<String> allKeys = new HashSet<>();
-        allKeys.addAll(dict1.keySet());
-        allKeys.addAll(dict2.keySet());
 
-        //Сортировка в алфавитном порядке
-        List<String> sortedKeys = new ArrayList<>(allKeys);
-        Collections.sort(sortedKeys);
-
-        LinkedHashMap<String, String> diffMap = new LinkedHashMap<>();
-        for (String key: sortedKeys
-             ) {
-            Object value1 = dict1.get(key);
-            Object value2 = dict2.get(key);
-
-            if (value1 == null && value2 != null) {
-                diffMap.put(key, "added");
-            } else if (value1 != null && value2 == null) {
-                diffMap.put(key, "deleted");
-            } else if (value1 != null && value2 != null) {
-                if (value1.equals(value2)) {
-                    diffMap.put(key, "unchanged");
-                } else {
-                    diffMap.put(key, "changed");
-                }
-            }
-        }
-        return diffMap;
+    //решение Stream API
+    public static LinkedHashMap<String, String> genDiff(Map<String, ?> map1, Map<String, ?> map2) {
+        return Stream.concat(map1.entrySet().stream(), map2.entrySet().stream())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> {
+                            if (!map1.containsKey(entry.getKey())) {
+                                return "added";
+                            } else if (!map2.containsKey(entry.getKey())) {
+                                return "deleted";
+                            } else if (map1.get(entry.getKey()).equals(map2.get(entry.getKey()))) {
+                                return "unchanged";
+                            } else {
+                                return "changed";
+                            }
+                        },
+                        (v1, v2) -> v1,
+                        LinkedHashMap::new
+                ));
     }
+    //решение в лоб
+//    public static LinkedHashMap<String, String>  genDiff(Map<String, ?> dict1,
+//                            Map<String, ?> dict2) {
+//        //Объединенное множество ключей из двух словарей
+//        Set<String> allKeys = new HashSet<>();
+//        allKeys.addAll(dict1.keySet());
+//        allKeys.addAll(dict2.keySet());
+//
+//        //Сортировка в алфавитном порядке
+//        List<String> sortedKeys = new ArrayList<>(allKeys);
+//        Collections.sort(sortedKeys);
+//
+//        LinkedHashMap<String, String> diffMap = new LinkedHashMap<>();
+//        for (String key: sortedKeys
+//             ) {
+//            Object value1 = dict1.get(key);
+//            Object value2 = dict2.get(key);
+//
+//            if (value1 == null && value2 != null) {
+//                diffMap.put(key, "added");
+//            } else if (value1 != null && value2 == null) {
+//                diffMap.put(key, "deleted");
+//            } else if (value1 != null && value2 != null) {
+//                if (value1.equals(value2)) {
+//                    diffMap.put(key, "unchanged");
+//                } else {
+//                    diffMap.put(key, "changed");
+//                }
+//            }
+//        }
+//        return diffMap;
+//    }
 
 //
 //    public static boolean isAdded(Map<String, Object> map1,
