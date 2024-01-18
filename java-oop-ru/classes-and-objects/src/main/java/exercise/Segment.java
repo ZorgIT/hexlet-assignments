468414431024f857819e9f10272a9d90d2293499
Zorgit <piligrimexe@gmail.com>
2024-01-18 20:02:02 +0300

    Fill Point and Segment class

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -1,5 +1,30 @@
 package exercise;
 
 // BEGIN
+public class Segment {
+    Point begin;
+    Point end;
 
+    public Segment(Point begin, Point end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public Point getBeginPoint() {
+        return begin;
+    }
+
+    public Point getEndPoint() {
+        return end;
+    }
+
+    public Point getMidPoint() {
+        int x1 = begin.getX();
+        int y1 = begin.getY();
+        int x2 = end.getX();
+        int y2 = end.getY();
+        return new Point(Math.round((x1 + x2) / 2),
+                Math.round((y1 + y2) / 2));
+    }
+}
 // END
