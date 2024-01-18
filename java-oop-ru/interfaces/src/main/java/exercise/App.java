83872644ce85a6dc98f86516fd3311b3228301f9
Zorgit <piligrimexe@gmail.com>
2024-01-18 22:21:44 +0300

    Implement Home, Flat and Cottage

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -1,8 +1,17 @@
 package exercise;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 // BEGIN
-
+public class App {
+    public static List<String> buildApartmentsList(List<Home> apartments,
+                                                   int count) {
+        return apartments.stream().sorted(Comparator.comparing(x -> x.getArea()))
+                .limit(count)
+                .map(Object::toString)
+                .collect(Collectors.toList());
+    }
+}
 // END
--- java-oop-ru/interfaces/src/main/java/exercise/Home.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Home.java
@@ -1,5 +1,12 @@
 package exercise;
 
 // BEGIN
+public interface Home {
+    double getArea();
 
+    default double compareTo(Home another) {
+        return this.getArea() == another.getArea() ? 0 :
+                (this.getArea() > another.getArea() ? 1 : -1);
+    }
+}
 // END
