bb2f18003261b66c351381d73f3592ce949fa3f9
Zorgit <piligrimexe@gmail.com>
2024-06-07 02:35:45 +0300

    add base solve

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,9 +1,39 @@
 package exercise;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 // BEGIN
+public class App {
+    public static void main(String[] args) {
+        KeyValueStorage storage = new InMemoryKV(Map.of("key", "10"));
+        // Получение значения по ключу
+        storage.get("key", "default"); // "10"
+        storage.get("unknown", "default"); // "default"
+        // Установка нового значения
+        storage.set("key2", "value2");
+        storage.get("key2", "default"); // "value2"
+        // Удаление ключа
+        storage.unset("key2");
+        storage.get("key2", "default"); // "default"
+        // Получение всех данных из базы
+        Map<String, String> data = storage.toMap();
+        System.out.println(data); // => {key=10};
+    }
 
+    public static void swapKeyValue(KeyValueStorage storages) {
+        Map<String, String> data = storages.toMap();
+        Map<String, String> swapped = new HashMap<>();
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            swapped.put(entry.getValue(), entry.getKey());
+            storages.unset(entry.getKey());
+        }
+        for (Map.Entry<String, String> entry : swapped.entrySet()) {
+            storages.set(entry.getKey(), entry.getValue());
+        }
+    }
+}
 // END
--- java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -4,5 +4,40 @@ import java.util.Map;
 import java.util.HashMap;
 
 // BEGIN
+public class InMemoryKV implements KeyValueStorage {
 
+
+    private HashMap<String, String> memoryDB;
+
+    public InMemoryKV(Map<String, String> memoryDB) {
+        this.memoryDB =  new HashMap<>(memoryDB);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        memoryDB.put(key,value);
+        //memoryDB.putIfAbsent(key, value);
+    }
+
+    @Override
+    public void unset(String key) {
+        if (memoryDB.containsKey(key)) {
+            memoryDB.remove(key);
+        }
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        if (memoryDB.containsKey(key)) {
+            return memoryDB.get(key);
+        }
+
+        return defaultValue;
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        return new HashMap<>(memoryDB);
+    }
+}
 // END
