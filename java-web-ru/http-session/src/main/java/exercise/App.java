891ab1912009f8ce8bacf4886805889ce517fd71
Zorgit <piligrimexe@gmail.com>
2024-08-01 21:18:47 +0300

    fix page output

--- java-web-ru/http-session/src/main/java/exercise/App.java
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -1,6 +1,8 @@
 package exercise;
 
 import io.javalin.Javalin;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,11 +17,21 @@ public final class App {
         });
 
         // BEGIN
-        
+        app.get("users", ctx -> {
+            var pageNum =
+                    ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
+            var perPageNum =
+                    ctx.queryParamAsClass("per", Integer.class).getOrDefault(5);
+            List<Map<String, String>> result = new ArrayList<>();
+            var lowerBound = pageNum * perPageNum - perPageNum;
+            var upperBound = lowerBound + perPageNum;
+            for (int i = lowerBound; i < upperBound; i++) {
+                result.add(USERS.get(i));
+            }
+            ctx.json(result);
+        });
         // END
-
         return app;
-
     }
 
     public static void main(String[] args) {
