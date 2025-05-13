c6429bb56085d6930f8eaa51f562bc38d0ab3db0
Zorgit <piligrimexe@Gmail.com>
2025-05-14 00:16:14 +0300

    Base crud app homework

---
+++ java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
@@ -0,0 +1,84 @@
+package exercise;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+
+    @PostMapping("/posts")
+    public Post create(@RequestBody Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    @GetMapping("/posts")
+    public List<Post> list(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int limit) {
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (limit < 1) {
+            limit = 10;
+        }
+
+        int offset = (page - 1) * limit;
+
+        return posts.stream()
+                .skip(offset)
+                .limit(limit)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/posts/{id]")
+    public Optional<Post> show(@PathVariable Integer id) {
+        return posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post update(@PathVariable Integer id, @RequestBody Post data) {
+        var maybePost = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (maybePost.isPresent()) {
+            var post = maybePost.get();
+            post.setId(post.getId());
+            post.setTitle(post.getTitle());
+            post.setBody(post.getBody());
+        }
+        return data;
+    }
+
+    @DeleteMapping("/pages/{id")
+    public void delete(@PathVariable Integer id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+    // END
+}
