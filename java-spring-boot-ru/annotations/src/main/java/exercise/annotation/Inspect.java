67f0aba17bb142a08af75515099358d03635e9fe
@hexlet/cli <support@hexlet.io>
2025-05-13 22:43:01 +0300

    submit java-spring-boot-ru/annotations

--- java-spring-boot-ru/annotations/src/main/java/exercise/annotation/Inspect.java
+++ java-spring-boot-ru/annotations/src/main/java/exercise/annotation/Inspect.java
@@ -11,16 +11,3 @@ import java.lang.annotation.Target;
 public @interface Inspect {
 
 }
-// END
-
-
-/*
-
-@Retention(RetentionPolicy.RUNTIME) //жизненный цикл аннотации (как долго
-// остается с кодом. В указанном случае - будет жить во время выполнения
-@Target(ElementType.METHOD) // Где применяется аннотация (в методах)
-public @interface LogExecutionTime {
-    boolean enabled();
-    long threshold() default 0; // пороговое в мс
-}
- */
