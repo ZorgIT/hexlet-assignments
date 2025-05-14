22dc62bad5272282071d333bed7fe085fc151409
Zorgit <piligrimexe@Gmail.com>
2025-05-14 23:54:55 +0300

    Create crud with response entity

--- java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
@@ -2,6 +2,7 @@ package exercise;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,14 +25,56 @@ import lombok.Setter;
 public class Application {
     // Хранилище добавленных постов
     @Setter
-    private static  List<Post> posts = Data.getPosts();
+    private static List<Post> posts = Data.getPosts();
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     // BEGIN
-    
+    //create
+    @PostMapping("/posts")
+    public ResponseEntity<Post> create(@RequestBody Post post) {
+        posts.add(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(post);
+    }
+
+    //read
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> show(@RequestParam(defaultValue = "10") int limit) {
+        var result = posts.stream().limit(limit).toList();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(posts.size()))
+                .body(result);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> get(@PathVariable String id) {
+        Optional<Post> post =
+                posts.stream()
+                        .filter(p -> p.getId().equals(id))
+                        .findFirst();
+        return post.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //update
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<Post> update(@PathVariable String id,
+                                       @RequestBody Post body) {
+        var maybePost = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (maybePost.isPresent()) {
+            var post = maybePost.get();
+            post.setTitle(body.getTitle());
+            post.setBody(body.getBody());
+            return ResponseEntity.ok(post);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
     // END
 
     @DeleteMapping("/posts/{id}")
