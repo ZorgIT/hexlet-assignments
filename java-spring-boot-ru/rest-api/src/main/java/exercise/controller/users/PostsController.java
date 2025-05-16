a1a26518aac00d90fd4f79060bac1d4de10f8c56
Zorgit <piligrimexe@Gmail.com>
2025-05-16 21:34:09 +0300

    Post controller improved

--- java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,7 +1,11 @@
 package exercise.controller.users;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,5 +18,43 @@ import exercise.model.Post;
 import exercise.Data;
 
 // BEGIN
+@RestController
+@RequestMapping("/api/users")
+public class PostsController {
 
+    private List<Post> posts = new ArrayList<>();
+
+    @GetMapping("/{id}/posts")
+    public ResponseEntity<List<Post>> getUserPosts(@PathVariable long id) {
+        List<Post> userPosts = posts.stream()
+                .filter(p -> p.getUserId() == id)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(userPosts);
+    }
+
+    @PostMapping("/{id}/posts")
+    public ResponseEntity<Post> createUserPost(
+            @PathVariable int id,
+            @RequestBody Map<String, String> postData) {
+
+        if (!postData.containsKey("slug") ||
+                !postData.containsKey("title") ||
+                !postData.containsKey("body")) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Post newPost = new Post();
+        newPost.setUserId(id);
+        newPost.setSlug(postData.get("slug"));
+        newPost.setTitle(postData.get("title"));
+        newPost.setBody(postData.get("body"));
+
+        posts.add(newPost);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(newPost);
+    }
+}
 // END
