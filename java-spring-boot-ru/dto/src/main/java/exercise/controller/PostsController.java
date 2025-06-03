ec307fdedbcb94aee826fe52644a5a52e5f83851
@hexlet/cli <support@hexlet.io>
2025-06-03 22:41:02 +0300

    submit java-spring-boot-ru/dto

--- java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -1,11 +1,16 @@
 package exercise.controller;
 
+import exercise.model.Comment;
 import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import exercise.model.Post;
@@ -15,5 +20,60 @@ import exercise.dto.PostDTO;
 import exercise.dto.CommentDTO;
 
 // BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
+    @GetMapping
+    public ResponseEntity<List<PostDTO>> showPosts() {
+        List<Post> posts = postRepository.findAll();
+        List<PostDTO> postDTOs = new ArrayList<>();
+        for (Post post : posts) {
+            PostDTO postDTO = new PostDTO();
+
+            postDTO.setId(post.getId());
+            postDTO.setTitle(post.getTitle());
+            postDTO.setBody(post.getBody());
+            List<Comment> comments = commentRepository.findByPostId(post.getId());
+            ArrayList<CommentDTO> commentDTOs = new ArrayList<>();
+            for (Comment comment : comments) {
+                CommentDTO commentDTO = new CommentDTO();
+                commentDTO.setId(comment.getId());
+                commentDTO.setBody(comment.getBody());
+                commentDTOs.add(commentDTO);
+            }
+            postDTO.setComments(commentDTOs);
+            postDTOs.add(postDTO);
+        }
+        return new ResponseEntity<>(postDTOs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> showPost(@PathVariable Long id) {
+        Post post = postRepository.findById(id).orElse(null);
+        PostDTO postDTO = new PostDTO();
+        if (post == null) {
+            throw new ResourceNotFoundException("Post with id " + id + " not found");
+        } else {
+            postDTO.setId(post.getId());
+            postDTO.setTitle(post.getTitle());
+            postDTO.setBody(post.getBody());
+            List<Comment> comments = commentRepository.findByPostId(post.getId());
+            ArrayList<CommentDTO> commentDTOs = new ArrayList<>();
+            for (Comment comment : comments) {
+                CommentDTO commentDTO = new CommentDTO();
+                commentDTO.setId(comment.getId());
+                commentDTO.setBody(comment.getBody());
+                commentDTOs.add(commentDTO);
+            }
+            postDTO.setComments(commentDTOs);
+        }
+        return new ResponseEntity<>(postDTO, HttpStatus.OK);
+    }
+
+}
 // END
--- java-spring-boot-ru/dto/src/main/java/exercise/dto/PostDTO.java
+++ java-spring-boot-ru/dto/src/main/java/exercise/dto/PostDTO.java
@@ -1,9 +1,24 @@
 package exercise.dto;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-// BEGIN
+import static jakarta.persistence.GenerationType.IDENTITY;
 
+// BEGIN
+@Getter
+@Setter
+public class PostDTO {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String title;
+    private String body;
+    private ArrayList<CommentDTO> comments;
+
+}
 // END
