db1a39cc90b33bddba19eb1bc599acd1a0a5aab4
@hexlet/cli <support@hexlet.io>
2025-06-10 23:32:22 +0300

    submit java-spring-boot-ru/dto-to-entity

--- java-spring-boot-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
+++ java-spring-boot-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -1,12 +1,9 @@
 package exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import exercise.model.Contact;
 import exercise.repository.ContactRepository;
@@ -21,6 +18,33 @@ public class ContactsController {
     private ContactRepository contactRepository;
 
     // BEGIN
-    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDTO create(@RequestBody ContactCreateDTO contactData) {
+        var contact = toEntity(contactData);
+        contactRepository.save(contact);
+        var contactDTO = toDTO(contact);
+        return contactDTO;
+    }
+
+    private Contact toEntity(ContactCreateDTO contactData) {
+        var contact = new Contact();
+        contact.setPhone(contactData.getPhone());
+        contact.setFirstName(contactData.getFirstName());
+        contact.setLastName(contactData.getLastName());
+        return contact;
+    }
+
+    private ContactDTO toDTO(Contact contact) {
+        var contactDTO = new ContactDTO();
+        contactDTO.setId(contact.getId());
+        contactDTO.setPhone(contact.getPhone());
+        contactDTO.setFirstName(contact.getFirstName());
+        contactDTO.setLastName(contact.getLastName());
+        contactDTO.setCreatedAt(contact.getCreatedAt());
+        contactDTO.setUpdatedAt(contact.getUpdatedAt());
+        return contactDTO;
+    }
+
     // END
 }
