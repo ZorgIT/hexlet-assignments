a6c97692568515a5374cb7e36fb9a050a4f70969
@hexlet/cli <support@hexlet.io>
2025-08-02 10:53:10 +0300

    submit java-spring-boot-ru/search

--- java-spring-boot-ru/search/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-boot-ru/search/src/main/java/exercise/controller/ProductsController.java
@@ -1,7 +1,5 @@
 package exercise.controller;
 
-import java.util.List;
-
 import exercise.dto.ProductCreateDTO;
 import exercise.dto.ProductDTO;
 import exercise.dto.ProductParamsDTO;
@@ -9,6 +7,7 @@ import exercise.dto.ProductUpdateDTO;
 import exercise.mapper.ProductMapper;
 import exercise.specification.ProductSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,6 +25,8 @@ import exercise.exception.ResourceNotFoundException;
 import exercise.repository.ProductRepository;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
@@ -36,7 +37,22 @@ public class ProductsController {
     private ProductMapper productMapper;
 
     // BEGIN
-    
+
+    @Autowired
+    private ProductSpecification productSpecBuilder;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductDTO> index(ProductParamsDTO params,
+                                  @RequestParam(defaultValue = "1") int page) {
+        var spec = productSpecBuilder.build(params);
+
+        var products = productRepository.findAll(spec, PageRequest.of(page - 1,
+                10));
+        return products.stream()
+                .map(productMapper::map)
+                .toList();
+    }
     // END
 
     @PostMapping("")
--- java-spring-boot-ru/search/src/main/java/exercise/specification/ProductSpecification.java
+++ java-spring-boot-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -7,5 +7,64 @@ import exercise.dto.ProductParamsDTO;
 import exercise.model.Product;
 
 // BEGIN
+@Component
+public class ProductSpecification {
+    public Specification<Product> build(ProductParamsDTO params) {
+        return withCategoryId(params.getCategoryId())
+                .and(withPriceGt(params.getPriceGt()))
+                .and(withPriceLt(params.getPriceLt()))
+                .and(withRatingGt(params.getRatingGt()))
+                .and(withTitleCont(params.getTitleCont()));
+    }
 
+    public Specification<Product> withCategoryId(Long categoryId) {
+        return (root, query, criteriaBuilder) -> {
+            if (categoryId == null) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("category").get("id"), categoryId);
+        };
+    }
+
+    public Specification<Product> withPriceGt(Integer priceGt) {
+        return ((root, query, criteriaBuilder) -> {
+            if (priceGt == null) {
+                return null;
+            }
+            return criteriaBuilder.greaterThan(root.get("price"), priceGt);
+        });
+    }
+
+    public Specification<Product> withPriceLt(Integer priceGt) {
+        return ((root, query, criteriaBuilder) -> {
+            if (priceGt == null) {
+                return null;
+            }
+            return criteriaBuilder.lessThan(root.get("price"), priceGt);
+        });
+    }
+
+    public Specification<Product> withRatingGt(Double ratingGt) {
+        return ((root, query, criteriaBuilder) -> {
+            if (ratingGt == null) {
+                return null;
+            }
+            return criteriaBuilder.greaterThan(root.get("rating"), ratingGt);
+        });
+    }
+
+    public Specification<Product> withTitleCont(String titleCont) {
+        return (root, query, criteriaBuilder) -> {
+            if (titleCont == null) {
+                return null;
+            }
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("title")),
+                    "%" + titleCont.toLowerCase() + "%"
+            );
+        };
+    }
+
+
+}
 // END
